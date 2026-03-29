cc11c1cbb14ce363c4014d26d0445df49038a8b6
alfba01 <alfredo.baena@gmail.com>
2026-03-29 20:08:32 +0000

    Cleanup: Remove unused import from serializers.py and ensure formatting for checks

--- octofit-tracker/backend/octofit_tracker/core/serializers.py
+++ octofit-tracker/backend/octofit_tracker/core/serializers.py
@@ -1,6 +1,5 @@
 from rest_framework import serializers
 from core.models import Team, User, Activity, Leaderboard, Workout
-from bson import ObjectId
 
 class TeamSerializer(serializers.ModelSerializer):
     class Meta:
